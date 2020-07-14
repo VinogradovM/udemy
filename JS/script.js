699677f1b31f276f519fc05720cd3144f35fff88
Michael Vinogradov <123.vertigo.meizu@gmail.com>
2020-07-14 16:08:25 +0300

    add switch construction

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+"use strict"
+/* let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -7,7 +8,6 @@ let personalMovieDB = {
     genres: [],
     privat: false 
 };
-
 
 let a = prompt("Один из последних просмотренных фильмов?", ""),
     b = prompt("На сколько оцените его?", ""),
@@ -18,4 +18,31 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+ */
 
+
+const num = 50;
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100){
+    console.log('Много');
+}else {
+    console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error!');
+
+
+const number = 50;
+
+switch (number) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
