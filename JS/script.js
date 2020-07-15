bd714053329df934b3de0faa9699b79056a2f1b8
Michael Vinogradov <123.vertigo.meizu@gmail.com>
2020-07-15 17:01:43 +0300

    add construction and cycles to choose variants

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 "use strict"
-/* let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+ let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -9,66 +9,31 @@ let personalMovieDB = {
     privat: false 
 };
 
-let a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//автоматизирование вопросов
+
+for (let i = 0; i < 2; i++){
+
+       let a = prompt("Один из последних просмотренных фильмов?", "");
+       let b = prompt("На сколько оцените его?", "");
+
+       if (a != null && b != null && a != '' && b != '' && a.length < 50 &&  b.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("done")
+       } else {
+            console.log("error")
+            i--;
+       }    
+} 
+
+if (personalMovieDB.count < 10 ) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+    console.log("Вы классический зритель")
+} else if (personalMovieDB.count >=30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Ошибка")
+}
 
 console.log(personalMovieDB);
- */
-
-
-
-/* Условия */
-
-/* const num = 50;
-if (num < 49) {
-    console.log('Error');
-} else if (num > 100){
-    console.log('Много');
-}else {
-    console.log('Ok!');
-}
-
-(num === 50) ? console.log('Ok!') : console.log('Error!');
-
-
-const number = 50;
-
-switch (number) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
-} */
-
-
-/* Циклы */
-let num = 50;
-
-/* while (num <= 55) {
-    console.log(num);
-    num++;
-} */
-
-/* do {
-    console.log(num);
-    num++;
-} while (num < 55);
- */
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-       // break; //прерывает на 6
-        continue; // продолжает работу исключая 6
-    }
-    console.log(i);
-}
