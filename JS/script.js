da94f07bd6cfc99b5f5daa16d8c61717516fe986
Michael Vinogradov <123.vertigo.meizu@gmail.com>
2020-07-18 17:43:42 +0300

    all functions combined into a single object

--- JS/script.js
+++ JS/script.js
@@ -1,69 +1,70 @@
 "use strict"
- let numberOfFilms;
-
- function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
- }
- start();
 
 
-let personalMovieDB = {
-    count: numberOfFilms,
+const personalMovieDB = {
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false 
-};
-
-//автоматизирование вопросов
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++){
-        let a = prompt("Один из последних просмотренных фильмов?", "");
-        let b = prompt("На сколько оцените его?", "");
- 
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 &&  b.length < 50) {
-             personalMovieDB.movies[a] = b;
-             console.log("done")
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        }
+    },
+     rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++){
+            let a = prompt("Один из последних просмотренных фильмов?", "");
+            let b = prompt("На сколько оцените его?", "");
+     
+            if (a != null && b != null && a != '' && b != '' && a.length < 50 &&  b.length < 50) {
+                 personalMovieDB.movies[a] = b;
+                 console.log("done");
+            } else {
+                 console.log("error");
+                 i--;
+            }    
+        }
+    },
+    detectePersonalLevel: function() {
+        if (personalMovieDB.count < 10 ) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >=30) {
+            console.log("Вы киноман");
         } else {
-             console.log("error")
-             i--;
-        }    
- } 
-}
-rememberMyFilms();
+            console.log("Ошибка");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+     toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+     writeYouGenres: function(){
+         for (let i = 1; i<= 3; i++){  
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-function detectePersonalLevel() {
-    if (personalMovieDB.count < 10 ) {
-        alert("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
-        console.log("Вы классический зритель")
-    } else if (personalMovieDB.count >=30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Ошибка")
+            if (genre == "" || genre == null) {
+                console.log('Введите корректные данные');
+                i--;
+            } else {
+                personalMovieDB.genres[i -1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
-}
 
-detectePersonalLevel();
-
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
- }
- showMyDB ();
-
-
- function writeYouGenres(){
-    for (let i=1; i < 4; i++) {       
-        personalMovieDB.genres[i - 1] = prompt("Ваш любимый жанр под номером " + i + '');
-    }
- }
- writeYouGenres();
+};
